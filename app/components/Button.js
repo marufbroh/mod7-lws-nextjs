35715bce2ab69c1ecebd2e30b20e8a677efa0870
Tamim Iqbal Maruf <marufmd775@gmail.com>
2024-07-17 16:16:54 +0600

    useRouter hook

--- app/components/Button.js
+++ app/components/Button.js
@@ -1,19 +1,20 @@
 "use client";
+
 import { useRouter } from "next/navigation";
 
-export default function Button({ children }) {
-    const router = useRouter();
+const Button = ({ children }) => {
+  const router = useRouter();
+  const handleClick = () => {
+    router.push("/dashboard/analytics");
+  };
+  return (
+    <button
+      onClick={handleClick}
+      className="mt-6 bg-blue-500 px-3 py-2 rounded text-gray-50"
+    >
+      {children}
+    </button>
+  );
+};
 
-    const handleClick = () => {
-        router.push("/dashboard/analytics");
-    };
-
-    return (
-        <button
-            onClick={handleClick}
-            className="mt-6 bg-blue-500 px-3 py-2 rounded text-gray-50"
-        >
-            {children}
-        </button>
-    );
-}
+export default Button;
