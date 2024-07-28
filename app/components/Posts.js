0c5fdcc0e9e6dd706f7a16165a1e3c2983553ce4
Tamim Iqbal Maruf <marufmd775@gmail.com>
2024-07-28 13:13:00 +0600

    Streaming nextjs

---
+++ app/components/Posts.js
@@ -0,0 +1,11 @@
+
+const Posts = () => {
+  return (
+    <div>
+      lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum
+      lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum
+    </div>
+  );
+};
+
+export default Posts;
--- app/heavy/page.js
+++ app/heavy/page.js
@@ -1,11 +1,17 @@
-import React from 'react';
+import React, { Suspense } from "react";
+import Posts from "../components/Posts";
 
 const Heavy = () => {
-    return (
-        <div>
-            <h1>Heavy Page</h1>
-        </div>
-    );
+  return (
+    <div>
+      <h1>Heavy Page</h1>
+      <div>
+        <Suspense fallback={<h1>Loading posts.....</h1>}>
+          <Posts />
+        </Suspense>
+      </div>
+    </div>
+  );
 };
 
 export default Heavy;
